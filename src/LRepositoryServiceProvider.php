710c3cdd675b288533bc42f98cd2efa63508903c
vitoutry <vitoutry@gmail.com>
2020-06-14 23:47:37 +0700

    first commit

---
+++ src/LRepositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vendor\LRepository;
+
+use Illuminate\Support\ServiceProvider;
+
+class LRepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
